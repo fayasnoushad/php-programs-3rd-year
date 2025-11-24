76ad7a3d1713e07d2f11efc4102dc525201cb238
Fayas <thefayasnoushad@gmail.com>
2025-11-24 14:59:09 +0530

    update fibonacci program

--- fibonacci.php
+++ fibonacci.php
@@ -1,3 +1,8 @@
+<!-- Design an HTML page that includes a form containing an input element of text type
+and accepts a numberas input and a submit button with caption Generate Fibonacci Series.
+Include PHP scripts in the HTML page so that, when the user clicks the submit button,
+Fibonacci series up to the number entered by the user is displayed.-->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,8 +16,8 @@
     <h1>Fibonacci Series</h1>
     <form method="post">
         <label for="number">Enter a number: </label>
-        <input type="number" name="number" />
-        <input type="submit" value="Get Series" />
+        <input type="text" name="number" />
+        <input type="submit" value="Get Fibonacci Series" />
     </form>
 </body>
 
@@ -21,16 +26,19 @@
 <?php
 if ($_POST) {
     $number = $_POST['number'];
+    if (!is_numeric($number)) {
+        echo 'Enter a number';
+        return;
+    }
     echo 'Fibonacci Series: ';
     $first = 0;
     $second = 1;
     $next;
-    while ($number > -1) {
+    while ($first < $number) {
         echo "$first ";
         $next = $first + $second;
         $first = $second;
         $second = $next;
-        $number--;
     }
 }
 ?>
